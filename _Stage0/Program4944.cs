41172810fb0663e6a93b4db420a97192b4095323
Shirayt <Hand4d15@mbjcomp.org.il>
2024-10-15 13:42:50 +0300

    Initial code+ Welcome functions

---
+++ _Stage0/Program4944.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome4944();
+        Welcome2284();
+        Console.ReadKey();
+    }
+
+    private static void Welcome4944()
+    {
+        Console.WriteLine("Enter your name!");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, Welcome to Ayala and Shira's first project!", name);
+    }
+    static partial void Welcome2284();
+}

b017d791a044dd5254bfcc6563a1ddd1041e03d2
Shirayt <Hand4d15@mbjcomp.org.il>
2025-06-06 16:11:44 +0300

    Update MainWindow.xaml.cs

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace PL
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }

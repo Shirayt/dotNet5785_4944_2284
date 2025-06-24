b635b80b9e7760e6f55115f7fc5989dd87db0e67
Shirayt <Hand4d15@mbjcomp.org.il>
2025-06-24 18:37:27 +0300

    Stage 5- chapter 9 ג, ד, ה A single volunteer window
    chapter 10- Opening a single item view screen from the list view screen

---
+++ PL/Converters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PL
+{
+//    class ConvertUpdateToTrueKey : IValueConverter
+//    {
+//        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+//        { BO.Year year = (BO.Year)value; switch (year) { case BO.Year.FirstYear: return Brushes.Yellow; case BO.Year.SecondYear: return Brushes.Orange; case BO.Year.ThirdYear: return Brushes.Green; case BO.Year.ExtraYear: return Brushes.PaleVioletRed; case BO.Year.None: return Brushes.White; default: return Brushes.White; } }
+//        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+//            throw new NotImplementedException(); 
+//        }
+//    }
+//}
+
+    public class ConvertUpdateToCollapsedKey : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string buttonText = value as string;
+            return buttonText == "Add" ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
